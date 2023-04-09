2e4df75ccd8eb483113faa5e5d27701382fa2e36
shahzebnaqvi <shahzeb.naqvi12@gmail.com>
2023-04-09 06:53:16 +0500

    deployment

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -112,7 +112,7 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-deterministic = True
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
