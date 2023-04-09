aa7debe1030c658c52dfa94920084d2f6ac5c628
shahzebnaqvi <shahzeb.naqvi12@gmail.com>
2023-04-09 07:24:33 +0500

    deployment

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -40,13 +40,13 @@ INSTALLED_APPS = [
     'todo',
     'rest_framework',
 ]
-ALLOWED_ORIGINS = ['http://*', 'https://*','https://django-class-production.up.railway.app/']
+ALLOWED_ORIGINS = ['http://*', 'https://*','https://django-class-production.up.railway.app']
 CSRF_TRUSTED_ORIGINS = ALLOWED_ORIGINS.copy()
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
