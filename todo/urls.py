ce4f407821a002c7d451681ac315759aab709d5e
shahzebnaqvi <shahzeb.naqvi12@gmail.com>
2023-04-09 05:57:37 +0500

    routes adding

--- todo/urls.py
+++ todo/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('testing/', views.testing, name='testing'),
+    path("",views.ListTodoAPIView.as_view(),name="todo_list"),
+    path("create/", views.CreateTodoAPIView.as_view(),name="todo_create"),
+    path("update/<int:pk>/",views.UpdateTodoAPIView.as_view(),name="update_todo"),
+    path("delete/<int:pk>/",views.DeleteTodoAPIView.as_view(),name="delete_todo")
+
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from rest_framework.generics import CreateAPIView
+from rest_framework.generics import DestroyAPIView
+from rest_framework.generics import UpdateAPIView
+from todo.serializers import TodoSerializer
+from todo.models import Todo
+
 from django.http import HttpResponse
 
 def testing(request):
     return HttpResponse("Hello tesint!")
+
+# Create your views here.
+class ListTodoAPIView(ListAPIView):
+    """This endpoint list all of the available todos from the database"""
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+class CreateTodoAPIView(CreateAPIView):
+    """This endpoint allows for creation of a todo"""
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+class UpdateTodoAPIView(UpdateAPIView):
+    """This endpoint allows for updating a specific todo by passing in the id of the todo to update"""
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+class DeleteTodoAPIView(DestroyAPIView):
+    """This endpoint allows for deletion of a specific Todo from the database"""
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
