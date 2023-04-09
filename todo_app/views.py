0e7e1e339e4c2216c79bc48cfa94c2690b20e528
shahzebnaqvi <shahzeb.naqvi12@gmail.com>
2023-04-09 05:29:45 +0500

    deployment

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 def members(request):
-    
-    return HttpResponse("Hello world!")
-
+    now = datetime.now()
+    html = f'''
+    <html>
+        <body>
+            <h1>HelloWorld from Vercel!</h1>
+            <p>The current time is { now }.</p>
+        </body>
+    </html>
+    '''
+    return HttpResponse(html)
 def addToDo(request):
     return HttpResponse("Hello world! addTodo")
